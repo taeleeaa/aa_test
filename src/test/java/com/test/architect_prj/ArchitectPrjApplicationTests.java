c9caf4ffe7ce2ec5883546f636634a05dc526cb5
TaeKyeong <taekyeong_lee@naver.com>
2025-02-17 16:25:58 +0900

    init

---
+++ src/test/java/com/test/architect_prj/ArchitectPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.architect_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
