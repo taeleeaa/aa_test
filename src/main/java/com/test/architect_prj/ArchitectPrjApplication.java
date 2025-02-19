af17dbb379980e73c48f1c65d89dbb68d820d1d7
taeleeaa <taekyeong_lee@naver.com>
2025-02-19 13:44:35 +0900

    Update ArchitectPrjApplication.java

--- src/main/java/com/test/architect_prj/ArchitectPrjApplication.java
+++ src/main/java/com/test/architect_prj/ArchitectPrjApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArchitectPrjApplication {
+public class ArchitectPrjApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ArchitectPrjApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(ArchitectPrjApplication.class, args);
     }
